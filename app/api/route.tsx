21ea537676964c4f4d2374e3267e30d13e7ca62f
J <0xjariel@protonmail.com>
2023-09-27 21:32:05 -0600

    fetch and display 10 results

---
+++ app/api/route.tsx
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+import React from "react";
+const { google } = require("googleapis");
+
+export function GET(request: NextRequest) {
+    
+  return <div>route</div>;
+}
---
+++ app/components/DisplayList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const displayList = () => {
+  return <div>displayList</div>;
+};
+
+export default displayList;
